22ac666d37a39085ff91e7ba0427919be873d4ae
Mykyta_Kozhevnykov <Mykyta.Kozhevnykov@iii.com>
2017-10-04 10:41:58 +0300

    Added migrations functionality

--- WebAPITestApp/DBLayer/DbData/Product.cs
+++ WebAPITestApp/DBLayer/DbData/Product.cs
@@ -9,5 +9,6 @@ namespace DBLayer.DbData
         public int Id { get; set; }
         public string ProductName { get; set; }
         public string Description  { get; set; }
+        public double Costs { get; set; }
     }
 }
