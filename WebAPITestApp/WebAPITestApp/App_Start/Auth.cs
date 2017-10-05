6f7b02a46cb432b9e00dfef4673918461cae05da
Mykyta Kozhevnykov <Mykyta_Kozhevnykov@epam.com>
2017-10-05 23:39:11 +0300

    Dummy checkin

--- WebAPITestApp/WebAPITestApp/App_Start/Auth.cs
+++ WebAPITestApp/WebAPITestApp/App_Start/Auth.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
 
+[assembly: OwinStartup(typeof(WebAPITestApp.Startup))]
 namespace WebAPITestApp.App_Start
 {
-    public class Auth
+    public partial class Startup
     {
         public void ConfigureAuth(IApplicationBuilder app)
         {
-            
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            //app.UseWebApi(config);
         }
     }
 }
---
+++ WebAPITestApp/WebAPITestApp/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebAPITestApp.Controllers
+{
+    [Produces("application/json")]
+    [Microsoft.AspNetCore.Mvc.Route("Register")]
+    public class AccountController : Controller
+    {
+
+        //public async Task<IHttpActionResult> Register(RegisterBindingModel model)
+        //{
+
+        //}
+    }
+}
