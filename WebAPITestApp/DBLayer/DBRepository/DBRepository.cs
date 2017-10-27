093141e8052871c4454d2691a8ae050ce399c515
Mykyta_Kozhevnykov <Mykyta.Kozhevnykov@iii.com>
2017-10-27 18:14:55 +0300

    some other logging

--- WebAPITestApp/DBLayer/DBRepository/DBRepository.cs
+++ WebAPITestApp/DBLayer/DBRepository/DBRepository.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using NLogger;
 
 namespace DBLayer.DBRepository
 {
@@ -9,36 +11,75 @@ namespace DBLayer.DBRepository
     {
         protected DbContext Context;
         protected DbSet<T> DbSet;
+        private readonly ILoggerService _logger;
 
-        public DbRepository(DbContext context)
+        public DbRepository(DbContext context, ILoggerService logger)
         {
             Context = context;
             DbSet = Context.Set<T>();
+            _logger = logger;
         }
 
         public virtual void Create(T item)
         {
-            DbSet.Add(item);
+            try
+            {
+                DbSet.Add(item);
+            }
+            catch (Exception e)
+            {
+                _logger.Error("Was unable to add item {0} because of error {1}", item, e);
+            }
         }
 
         public virtual void Delete(T item)
         {
-            DbSet.Remove(item);
+            try
+            {
+                DbSet.Remove(item);
+            }
+            catch (Exception e)
+            {
+                _logger.Error("Was unable to remove item {0} because of error {1}", item, e);
+            }
         }
 
         public virtual async Task<T> GetItem(int id)
         {
-            return await DbSet.FindAsync(id);
+            try
+            {
+                return await DbSet.FindAsync(id);
+            }
+            catch (Exception e)
+            {
+                _logger.Error("Was unable to get item with {0} because of error {1}", id, e);
+            }
+            return null;
         }
 
         public virtual async Task<List<T>> GetAll()
         {
-            return await DbSet.ToListAsync();
+            try
+            {
+                return await DbSet.ToListAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.Error("Was unable to get all items because of error {0}", e);
+            }
+            return null;
         }
 
         public virtual void Update(T item)
         {
-            DbSet.Update(item);
+            try
+            {
+                DbSet.Update(item);
+            }
+            catch (Exception e)
+            {
+                _logger.Error("Was unable to update item {0} cause error {1}", item, e);
+            }
         }
     }
 }
