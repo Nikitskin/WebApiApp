eecbe5f676fe1e34cfd92f22dc82dbb0a70a2809
Mykyta_Kozhevnykov <Mykyta.Kozhevnykov@iii.com>
2017-12-13 19:49:14 +0200

    small adding

--- WebAPITestApp/WebAPITestApp/App_Start/AuthorizationServiceExtension.cs
+++ WebAPITestApp/WebAPITestApp/App_Start/AuthorizationServiceExtension.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
